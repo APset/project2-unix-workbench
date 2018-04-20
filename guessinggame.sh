672c0c16ef1afa2c664a4c10d61757dc6d1c1dbe
APset <adrpset@gmail.com>
2018-04-20 19:16:48 +0200

    Added files

---
+++ guessinggame.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+	
+	flag=0
+        number_files=$(ls . | wc -l)
+
+	function compa {
+                if [[ response -eq number_files ]]
+                then
+                        #echo
+                        text="CONGRATULATIONS, buddy!"
+                        flag=1
+
+                elif [[ response -gt number_files ]]
+                then
+                        text="There are LESS files in the current directory, try again"
+                        flag=0
+                else
+                        text="There are MORE files in the current directory, try again"
+                        flag=0
+                fi
+
+        } 
+	
+	echo "How many files do you thing there are in the current directory? (press INTRO after your response)"
+	
+	while [[ $flag -eq 0 ]]
+        do
+		read response
+		compa
+                echo $text
+
+        done
+
+
---
+++ makefile
@@ -0,0 +1,4 @@
+README.md: guessinggame.sh
+	echo "UNIX WORKBENCH - PEER GRADED ASSIGNMENT - A. BLANCO" > README.md
+	date >> README.md
+	wc -l guessinggame.sh >> README.md
